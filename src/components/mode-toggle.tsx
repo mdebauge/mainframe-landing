869e1dbac858c39aacfbe5724bad14282cae8394
Michel DeBauge <mdebau@gmail.com>
2024-04-03 14:21:40 -0700

    Hide focus on theme toggle button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -16,9 +16,13 @@ export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu className="focus-visible:outline-none">
+    <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="rounded-full" variant="ghost" size="icon">
+        <Button
+          className="rounded-full focus-visible:ring-0 focus-visible:ring-offset-0"
+          variant="ghost"
+          size="icon"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
