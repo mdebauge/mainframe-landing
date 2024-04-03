8fa2bb4ee358c90271cf2399fc700727a4de3805
Michel DeBauge <mdebau@gmail.com>
2024-04-03 10:49:55 -0700

    Add content to footer and remove main boilerplate

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="flex justify-center">
+      <div className="flex flex-col gap-x-4">
+        <small className="font-medium">
+          © 2024 Mainframe. All rights reserved.
+        </small>
+        <div className="flex gap-x-1 text-muted-foreground">
+          <small>
+            <Link href="/">Privacy Policy</Link>
+          </small>
+          <small>•</small>
+          <small>
+            <Link href="/">Terms and Conditions</Link>
+          </small>
+        </div>
+      </div>
+    </footer>
+  );
 }
