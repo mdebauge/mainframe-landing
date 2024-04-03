92b3a79a004cd68cdd2bc4112e900c6f7a8147f7
Michel DeBauge <mdebau@gmail.com>
2024-04-02 23:02:20 -0700

    Style theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="flex items-center justify-between h-14 px-3 sm:px-9 bg-background">
       <Link href="/">Mainframe</Link>
       <nav>
-        <ul className="flex gap-x-6">
+        <ul className="flex items-center gap-x-6">
           <li>
             <Link href="/">Log in</Link>
           </li>
