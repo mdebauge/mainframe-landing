f63c5188f05af3ff06645629c2040e7f68c94e8f
Michel DeBauge <mdebau@gmail.com>
2024-04-04 13:49:38 -0700

    Add mainframe logo svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-20 px-3 sm:px-9 bg-background">
-      <Link href="/">Mainframe</Link>
+      <Link href="/">
+        <Image
+          src="/mainframe-logo.svg"
+          alt="Mainframe logo"
+          width={174}
+          height={25}
+        />
+      </Link>
       <nav>
         <ul className="flex items-center gap-x-6">
           <li>
