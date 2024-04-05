20cb7280c52ffbce8de7f21f5e70a120e61d181b
Michel DeBauge <mdebau@gmail.com>
2024-04-04 20:14:31 -0700

    Add How It Works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,34 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-full mt-32 p-24 bg-background gap-x-12">
-      <div className="basis-2/4">
-        <h1 className="text-6xl font-bold mb-4">
-          Simplify your data ecosystem
-        </h1>
-        <h2 className="text-xl mb-4">
-          Connect and transform your data from any app or data source. Build
-          apps with all your data in one tool.
-        </h2>
-        <Button className="grow-0">Try it out</Button>
-      </div>
-      <div className="basis-2/4 flex justify-center items-center">
-        <Image
-          src="/mainframe-monitor.svg"
-          alt="Mainframe hero"
-          width={300}
-          height={300}
-        />
-      </div>
+    <main className="p-24 bg-background">
+      <section className="flex h-full mt-32 mb-16 gap-x-12">
+        <div className="basis-2/4">
+          <h1 className="text-6xl font-bold mb-4">
+            Simplify your data ecosystem
+          </h1>
+          <p className="text-xl mb-4">
+            Connect and transform your data from any app or data source. Build
+            apps with all your data in one tool.
+          </p>
+          <Button className="grow-0">Try it out</Button>
+        </div>
+        <div className="basis-2/4 flex justify-center items-center">
+          <Image
+            src="/mainframe-monitor.svg"
+            alt="Mainframe hero"
+            width={300}
+            height={300}
+          />
+        </div>
+      </section>
+      <section>
+        <h2 className="text-4xl text-center font-medium mb-8">How it works</h2>
+        <p className="text-lg text-center">
+          Learn how easy it is to connect your data and start building your app
+          within minutes
+        </p>
+      </section>
     </main>
   );
 }
