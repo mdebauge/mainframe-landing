ae07253220cd6c06edd5ff0da8842c46ac2c5fc9
Michel DeBauge <mdebau@gmail.com>
2024-04-06 12:20:19 -0700

    Fix header z-index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-16 px-3 sm:px-24 bg-background/80 backdrop-blur-xl">
+    <header className="fixed top-0 left-0 right-0 flex items-center justify-between h-16 px-3 sm:px-24 bg-background/80 backdrop-blur-xl z-10">
       <Link href="/">
         <svg
           className="fill-primary"
