23ae2820a219917d68d8713505c5f5228504080e
Michel DeBauge <mdebau@gmail.com>
2024-04-05 17:29:30 -0700

    Remove sticky footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 flex justify-center pb-2">
+    <footer className="flex justify-center pb-2">
       <div className="flex flex-col gap-x-4">
         <small className="">© 2024 Mainframe. All rights reserved.</small>
         <div className="flex gap-x-1 text-sm text-muted-foreground">
