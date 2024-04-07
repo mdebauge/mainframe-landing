792ca85a2db086d4c963c330b86e88f67eb07d13
Michel DeBauge <mdebau@gmail.com>
2024-04-07 13:32:51 -0700

    Style footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <footer className="flex justify-center pb-4">
       <div className="flex flex-col gap-x-4">
-        <small className="">© 2024 Mainframe. All rights reserved.</small>
+        <small className="text-muted-foreground">
+          © 2024 Mainframe. All rights reserved.
+        </small>
         <div className="flex justify-center gap-x-1 text-sm text-muted-foreground">
           <small>
             <Link href="/">Privacy Policy</Link>
