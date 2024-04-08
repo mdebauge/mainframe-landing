dae59cab2a6d33782ba7e00d7bd664a4258bd7cc
Michel DeBauge <mdebau@gmail.com>
2024-04-08 11:53:10 -0700

    Fix Step image type

--- src/components/step.tsx
+++ src/components/step.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 type StepProps = {
-  image: JSX.Element;
+  image: StaticImageData;
   header: string;
   blurb: string;
 };
